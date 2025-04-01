152ce0d3d4736f1b80882993ff94c6c655bb4b08
Boscoochen <61206389+Boscoochen@users.noreply.github.com>
2025-04-01 12:36:06 -0500

    fix qa2

--- qa/main.tf
+++ qa/main.tf
@@ -1,9 +1,9 @@
 module "qa" {
   source = "../modules/blog"
-  environment = {
+  Environment = {
     name = "qa"
     network_prefix = "10.1"
   }
-  asg_min = 0
-  asg_max = 0
+  asg_min_size = 0
+  asg_max_size = 0
 }
