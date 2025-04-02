48088b7a100fc34012a19daa808bb1e5313c2e45
Boscoochen <61206389+Boscoochen@users.noreply.github.com>
2025-04-02 14:54:18 -0500

    changed bucket name

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"  # replace with your actual bucket name
+    bucket         = "my-terraform-state-bucket-bosco-dev-2025"  # replace with your actual bucket name
     key            = "envs/dev/terraform.tfstate" # path inside the bucket
     region         = "us-west-2"
     encrypt        = true
